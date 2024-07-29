052fd27509a1f12860936050f5305abc1d984c8d
tadeo <tadeoacosta14@gmail.com>
2024-07-29 20:27:40 -0300

    copie y pegue

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
+import { AppRouter } from './router/AppRouter'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
 
-  const [nombre, setNombre] = useState('');
-  const [edad, setEdad] = useState('');
-  const [email, setEmail] = useState('');
-  const [contraseña, setContraseña] = useState('');
-
-  const enviarDatosAlBackend = async (nombre, edad, email, contraseña) => {
-    try {
-      const resp = await pruebaApi.post('auth/registro', {
-        nombre,
-        edad,
-        email,
-        contraseña,
-      });
-      console.log(resp);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (nombre === "" || edad === "" || email === "" || contraseña === "") {
-      console.log('todos los campos son obligatorios');
-    }
-    
-    enviarDatosAlBackend(nombre, edad, email, contraseña);
-  };
-
   return (
+    <>
+      <AppRouter />
+    </>
+  )
+}
 
-
+export default App
