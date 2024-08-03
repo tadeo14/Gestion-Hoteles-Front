d7b36fe494a971238c28c7041bd72337b3c1cad0
tadeo <tadeoacosta14@gmail.com>
2024-08-03 01:52:49 -0300

    listado de habitaciones para reservar

--- componentes/ListaHabitacionesUsuario.jsx
+++ componentes/ListaHabitacionesUsuario.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import Table from 'react-bootstrap/Table'; // Asegúrate de importar Table desde react-bootstrap
 import pruebaApi from '../src/api/pruebaApi';
 import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export const ListaHabitacionesUsuario = () => {
     const [habitaciones, setHabitaciones] = React.useState([]);
@@ -22,65 +22,55 @@ export const ListaHabitacionesUsuario = () => {
         getHabitaciones();
     }, []);
 
-
-
-
-
-
-  return (
-      <>
-          <div className='p-2'>
-          <h1 >  Listado de Habitaciones</h1>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Numero</th>
-            <th>Precio</th>
-            <th>Tipo</th>
-            
-          </tr>
-        </thead>
-                <tbody>
-                  {habitaciones.map((habitaciones) => {
-                      return (
-                          <tr>
-                              
-                            <td>{habitaciones._id}</td>
-                            <td>{habitaciones.numero}</td>
-                            <td>{habitaciones.precio}</td>
-                          <td>{habitaciones.tipo}</td>
-                          <td>
-                          <img src={`http://localhost:5173/public/images/${habitaciones.imagen}`} alt={`Habitación ${habitaciones.numero}`} style={{ width: '100px', height: 'auto' }} />
-                          </td>
-                          </tr>
-                      )
-                  })}
-                </tbody>
-          </Table>
-          <div>
-          <CardGroup>
+    return (
+        <>
+            <div className='p-2'>
+                <h1>Listado de Habitaciones</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Numero</th>
+                            <th>Precio</th>
+                            <th>Tipo</th>
+                            <th>Imagen</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                         {habitaciones.map((habitacion) => (
-                            <Card key={habitacion._id}>
-                                <Card.Img variant="left" src={`http://localhost:5173/public/images/${habitacion.imagen}`} alt={`Habitación ${habitacion.numero}`} style={{ width: '300px', height: 'auto' }}  />
-                                <Card.Body>
-                                    <Card.Title>Habitación {habitacion.numero}</Card.Title>
-                                    <Card.Text>
-                                        Tipo: {habitacion.tipo}<br />
-                                        Precio: ${habitacion.precio}
-                                    </Card.Text>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <small className="text-muted">Última actualización hace 3 minutos</small>
-                                </Card.Footer>
-                            </Card>
+                            <tr key={habitacion._id}>
+                                <td>{habitacion._id}</td>
+                                <td>{habitacion.numero}</td>
+                                <td>{habitacion.precio}</td>
+                                <td>{habitacion.tipo}</td>
+                                <td>
+                                    <img src={`http://localhost:5173/public/images/${habitacion.imagen}`} alt={`Habitación ${habitacion.numero}`} style={{ width: '100px', height: 'auto' }} />
+                                </td>
+                            </tr>
                         ))}
-                    </CardGroup>
-          </div>
-          </div>
-     
-    </>
-  );
+                    </tbody>
+                </Table>
+                <div>
+                    {habitaciones.map((habitacion) => (
+                        <Row key={habitacion._id} className="mb-3">
+                            <Col>
+                                <Card className="flex-row">
+                                    <Card.Img className="img-fluid" style={{ width: '200px', height: 'auto' }} src={`http://localhost:5173/public/images/${habitacion.imagen}`} alt={`Habitación ${habitacion.numero}`} />
+                                    <Card.Body>
+                                        <Card.Title>Habitación {habitacion.numero}</Card.Title>
+                                        <Card.Text>
+                                            Tipo: {habitacion.tipo}<br />
+                                            Precio: ${habitacion.precio}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default ListaHabitacionesUsuario;
