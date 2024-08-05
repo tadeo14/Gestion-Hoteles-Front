6bbc5165af5723967d9dfc9eaa0c98db99a6fc0b
tadeo <tadeoacosta14@gmail.com>
2024-08-05 02:23:32 -0300

    arrancamos con el front de reservas

--- src/pages/Usuario.jsx
+++ src/pages/Usuario.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 import ListadoReservasUsuario from '../../componentes/ListadoReservasUsuario';
 import ListaHabitacionesUsuario from '../../componentes/ListaHabitacionesUsuario';
+import RealizarReservas from '../../componentes/RealizarReservas';
+
 
 export const Usuario = () => {
   return (
     <>
       <div className='p-4'>
+      <RealizarReservas/>
       <ListadoReservasUsuario />
       <ListaHabitacionesUsuario/> 
       </div>
