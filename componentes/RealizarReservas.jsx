c36f7c1130efa01b6f670792f5795e37a3e6d851
tadeo <tadeoacosta14@gmail.com>
2024-08-31 22:38:53 -0300

    importamos row

--- componentes/RealizarReservas.jsx
+++ componentes/RealizarReservas.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Form, Button, Alert, Row, Col, Card } from 'react-bootstrap';
 import { jwtDecode } from "jwt-decode";
 
 import pruebaApi from '../src/api/pruebaApi';
